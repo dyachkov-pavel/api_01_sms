f06e14c555f70160a03e35e3b3cf46785af52ec6
dyachkov-pavel <pahanthebest12345@mail.ru>
2020-11-08 15:22:37 +0300

    raise exceptions

--- homework.py
+++ homework.py
@@ -6,7 +6,7 @@ from twilio.rest import Client
 import logging
 
 LOG_FORMAT = '%(levelname)s %(asctime)s - %(message)s'
-logging.basicConfig(level=logging.ERROR, 
+logging.basicConfig(level=logging.ERROR,
                     format=LOG_FORMAT)
 logger = logging.getLogger()
 
@@ -31,34 +31,36 @@ def get_status(user_id):
     vk_url = 'https://api.vk.com/method/'
     method = 'users.get'
     url = vk_url + method
-    status = requests.post(url, params=params).json()
-    return status['response'][0]['online']
+    try:
+        status = requests.post(url, params=params).json()
+        return status['response'][0]['online']
+    except requests.exceptions.HTTPError as errh:
+        logger.error(f'Http Error: {errh}')
+        raise errh
+    except requests.exceptions.ConnectionError as errc:
+        logger.error(f'Error Connecting: {errc}')
+        raise errc
+    except requests.exceptions.Timeout as errt:
+        logger.error(f'Timeout Error: {errt}')
+        raise errt
+    except requests.exceptions.RequestException as err:
+        logger.error(f'OOps: Something Else {err}')
+        raise err
 
-    
+
 def sms_sender(sms_text):
     message = TWILIO_CLIENT.messages.create(
-                                     body=sms_text,
-                                     from_=NUMBER_FROM,
-                                     to=NUMBER_TO
-                                    )
+        body=sms_text,
+        from_=NUMBER_FROM,
+        to=NUMBER_TO
+    )
     return message.sid
 
 
 if __name__ == '__main__':
     vk_id = input('Введите id ')
     while True:
-        try:
-            if get_status(vk_id) == 1:
-                sms_sender(f'{vk_id} сейчас онлайн!')
-                break
-        except requests.exceptions.HTTPError as errh:
-            logger.error(f'Http Error: {errh}')
-        except requests.exceptions.ConnectionError as errc:
-            logger.error(f'Error Connecting: {errc}')
-        except requests.exceptions.Timeout as errt:
-            logger.error(f'Timeout Error: {errt}')
-        except requests.exceptions.RequestException as err:
-            logger.error(f'OOps: Something Else {err}')
-        except Exception as e:
-            logger.error(f'Exception: {e}')
+        if get_status(vk_id) == 1:
+            sms_sender(f'{vk_id} сейчас онлайн!')
+            break
         time.sleep(5)
