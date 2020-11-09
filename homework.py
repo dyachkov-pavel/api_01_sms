1300c9604019cbc7e5c50b79bbf5a50885160166
dyachkov-pavel <pahanthebest12345@mail.ru>
2020-11-09 19:43:14 +0300

    1

--- homework.py
+++ homework.py
@@ -43,15 +43,15 @@ def get_status(user_id):
     except requests.exceptions.Timeout as errt:
         logger.error(f'Timeout Error: {errt}')
         raise errt
+    except KeyError as errk:
+        logger.error(f'Key Error: {errk}')
+        raise errk
+    except IndexError as erri:
+        logger.error(f'Index Error: {erri}')
+        raise erri
     except requests.exceptions.RequestException as err:
         logger.error(f'OOps: Something Else {err}')
         raise err
-    except KeyError as errk:
-        logger.error(f'OOps: Something Else {errk}')
-        raise errk
-    except IndexError as erri:
-        logger.error(f'OOps: Something Else {erri}')
-        raise erri
 
 
 def sms_sender(sms_text):
